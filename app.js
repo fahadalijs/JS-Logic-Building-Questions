39216e75c31d55d58a06fff3848b2a18c7423cde
fahadalijs <fahad9827022@gmail.com>
2024-03-08 10:57:08 +0500

    logic-building-paractice

---
+++ app.js
@@ -0,0 +1,284 @@
+// Question 1 answer
+// alert("Error! Please enter a valid passward")
+
+
+
+// Question 2 answer
+// alert("Welcome to JS land... \n Happy Coding!")
+
+
+
+// Question 4 answer
+
+// alert("Welcome to JS land...")
+// alert("Happy coding \n Prevelant this page form creating additional dilogs")
+
+
+// Question 4 answer
+
+// alert("Hello... I can run JS through my Web browser's consoles")
+// console.log("Hello... I can run JS through my Web browser's consoles")
+
+
+// // Question 7 answer
+// A
+// C
+
+
+
+
+// Variable for String
+
+// // Question 1 answer
+
+
+// var userName = "Fahad"
+// console.log(userName)
+
+// // Question 2 answer
+
+
+// var myName = "Fahad Ali"
+// console.log("my full name" + " " + myName)
+
+
+
+// // Question 3 answer
+
+
+// var tittleMessege = "Welcome to our website!"
+// console.log(tittleMessege)
+
+// // Question 4 answer
+
+
+// alert("Hello... World!")
+// var messege = "Hello... World!"
+// console.log(messege)
+
+// // Question 4 answer
+
+// var userName = "Fahad";
+// var age = 18;
+// var lastName = "Ali";
+// alert("my name is "  + userName + " " + "my age is" + " " + age  + " " +  "my last name is" + " "  + lastName);
+
+// // Question 5 answer
+
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// // Question 6 answer
+
+// var email = "example@gmail.com";
+// var alertmessege ="Your email is: " + email;
+// alert(alertmessege)
+
+// // Question 7 answer
+
+
+// var book = "A smarter \n way to learn JS";
+// alert("I am tring to learn from the book: " + book) ;
+
+// // Question 8 answer
+
+// document.write("<h1>Yah! i can write HTML content through JS<h1/>");
+
+// // Question 9 answer
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// Variables for Number
+
+// // Question 1 answer
+
+// var age = 18;
+// alert ("I am " + age + " year old");
+
+// // Question 2 answer
+
+// var number = 15;
+// alert("You have visited this site " + number + " times" );
+
+
+// // Question 3 answer
+
+// var birthyear = 2006;
+// document.write("<h3> My birthyear is " + birthyear + "<h3/>");
+
+// // Question 4 answer
+
+
+//   var visitorName = prompt("Welcome! Whats Your Name");
+//   var quantity = prompt("How many product would you like to ordered");
+//   var tittle = prompt("Which product would you like to ordered");
+
+
+//   document.write("<h1>" + visitorName + " ordered " + quantity + " " + tittle + " " + " on xyz clothing store <h1/>");
+
+
+// Variable names legal and illegal
+
+// // Question 1 answer
+
+// var Variable1, Variable2,Variable3;
+
+// // Question 2 answer
+
+
+// Legal variable names:
+
+// firstName
+// userAge
+// totalCost
+// _discountRate
+// camelCaseVariable
+
+// Illegal variable names:
+
+// 123variable (Cannot start with a number)
+// user-age (Cannot contain hyphens)
+// first name (Cannot contain spaces)
+// total#Cost (Cannot contain special characters except underscore)
+// class (Reserved keyword in JavaScript)
+
+
+
+// // Question 3 answer
+
+// document.write("<h1>rules of naming JS variable</h1>")
+// document.write("<p> Variable name can only contain letter ,number , underscors and Dollar sign</p>")
+// document.write("<p>Variable first begin with a letter , underscors or Dollar sign </p>")
+// document.write("<p> variable names are case sensitive</p>")
+// document.write("<p> Variable name should not be JS keyword </p>")
+
+
+
+
+// Math expresseions
+
+// // Question 1 answer
+
+// var num1 = 10;
+// var num2 = 20;
+
+// var num3 = num1 + num2;
+
+// document.write("<p>"+ num1 + "+" + num2 + " = " + num3 + "</p>" );
+  
+
+
+// // Question 2 answer
+
+
+// var num1 = 30;
+// var num2 = 20;
+
+// var num3 = num1 + num2;
+// var num4 = num1 - num2;
+// var num5 = num1 * num2;
+// var num6 = num1 % num2;
+// var num7 = num1 / num2;
+
+// document.write("<p>"+ num1 + "+" + num2 + "=" + num3 + "</p>");
+// document.write("<p>"+ num1 + "-" + num2 + "=" + num4 + "</p>");
+// document.write("<p>"+ num1 + " * " + num2 + " = " + num5 + "</p>");
+// document.write("<p>"+ num1 + " / " + num2 + " = " + num7 + "</p>");
+// document.write("<p>"+ num1 + " % " + num2 + " = " + num6 + "</p>");
+
+
+// // Question 3 answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
